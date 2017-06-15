1f23fcc8dfc6b5ddc0ea5a6e8cfd1f34a86dde81
Marissa Reinke <reinke.marissa@gmail.com>
2017-06-15 13:58:43 -0600

    Fix tests

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -85,13 +85,13 @@ describe('Everything', () => {
           description: 'testing route',
           start: '8',
           end: '10',
-        },
+        }
       )
       .end((error, response) => {
         response.should.have.status(201)
         response.body.should.be.a('object')
         response.body.should.have.property('title')
-        response.body.name.should.equal('test')
+        response.body.title.should.equal('test')
         chai.request(server)
         .get('/api/v1/events')
         .end((error, response) => {
@@ -112,8 +112,8 @@ describe('Everything', () => {
       .post('/api/v1/events')
       .send({
         description: 'testing',
-        start: 4,
-        end: 5
+        start: 'start',
+        end: 'end'
       })
       .end((err, response) => {
         response.should.have.status(422)
@@ -123,9 +123,9 @@ describe('Everything', () => {
       chai.request(server)
       .post('/api/v1/events')
       .send({
-        title: 'test',
-        start: 4,
-        end: 5
+        title: 'testing',
+        start: 'start',
+        end: 'end'
       })
       .end((err, response) => {
         response.should.have.status(422)
@@ -137,28 +137,13 @@ describe('Everything', () => {
       .send({
         title: 'testing',
         description: 'to test stuff',
-        end: 5
+        end: 'end'
       })
       .end((err, response) => {
         response.should.have.status(422)
         response.body.error.should.equal('Please include a start time')
         done()
       })
-
-      chai.request(server)
-      .post('/api/v1/events')
-      .send({
-        title: 'testing',
-        description: 'to test stuff',
-        start: 5
-      })
-      .end((err, response) => {
-        response.should.have.status(422)
-        response.body.error.should.equal('Please include an end time')
-        done()
-      })
     })
-
-
   })
 })
